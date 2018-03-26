a4ec785e6b6748682be8904f5af68ec0ab92c022
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-03-26 11:38:16 -0700

    Load configuration path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let configuration_path = load_configuration_path();
+    println!("Configuration path: {:?}", configuration_path);
+}
+
+fn load_configuration_path() -> PathBuf {
+    use std::env;
+    let folder = env::home_dir().unwrap_or(PathBuf::from("."));
+    folder.join(".ddnsimple.yaml")
 }
