e41234496e68b09f63997f873635cd0ad940b9ad
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-03-26 12:45:18 -0700

    Read current ip address

---
+++ src/current_ip.rs
@@ -0,0 +1,29 @@
+use regex::Regex;
+use reqwest;
+use reqwest::Response;
+use std::error::Error;
+use super::AppError;
+
+pub fn fetch() -> Result<String, AppError> {
+    fetch_ip_response()
+        .and_then(|response| read_text_from_response(response))
+        .and_then(|text| check_ip_in_text(text))
+}
+
+fn fetch_ip_response() -> Result<Response, AppError> {
+    reqwest::get("https://api.ipify.org").map_err(|error| AppError::NoResponse(error.description().to_owned()))
+}
+
+fn read_text_from_response(mut response: Response) -> Result<String, AppError> {
+    response.text().map_err(|error| AppError::MissingResponseText(error.description().to_owned()))
+}
+
+fn check_ip_in_text(text: String) -> Result<String, AppError> {
+    let text = text.trim();
+    let re = Regex::new(r"^\d+[.]\d+[.]\d+[.]\d+$").unwrap();
+    if re.is_match(text) {
+        Ok(text.to_owned())
+    } else {
+        Err(AppError::InvalidIpAddress(text.to_owned()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+extern crate regex;
+extern crate reqwest;
+
 use std::path::PathBuf;
+
+mod current_ip;
+
+#[derive(Debug)]
+pub enum AppError {
+    NoResponse(String),
+    MissingResponseText(String),
+    InvalidIpAddress(String),
+}
 
 fn main() {
     let configuration_path = load_configuration_path();
     println!("Configuration path: {:?}", configuration_path);
+
+    let current_ip = current_ip::fetch();
+    match current_ip {
+        Ok(ip) => println!("Ip: {}", ip),
+        Err(error) => println!("Error: {:?}", error),
+    }
 }
+
 
 fn load_configuration_path() -> PathBuf {
     use std::env;
